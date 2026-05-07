e9ac568297bbb9e132b8c216f3f1983ea5f70348
Ashima916 <ashimadutta9475@gmail.com>
2026-05-07 23:39:17 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyArsp-IQgfXJWS_53CC2Tmf9TPVfptofcU",
-  authDomain: "technobid-b4f64.firebaseapp.com",
-  projectId: "technobid-b4f64",
-  storageBucket: "technobid-b4f64.firebasestorage.app",
-  messagingSenderId: "76775701498",
-  appId: "1:76775701498:web:bd75bc2f1a192ce7b1c243",
-  measurementId: "G-C9JQM13ZHB"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
